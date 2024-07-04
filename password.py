93fb1b46a4a72cd19a66eaf5a0b7747565638187
Jannis Lauer <jannislauer@icloud.com>
2024-07-04 13:58:05 +0200

    Update password.py

--- password.py
+++ password.py
@@ -1,13 +1,19 @@
 import random as rd
+import time
 
-def listAlphabet():return [chr(i) for i in range(ord("a"), ord("z") + 1)]
+listAlphabet = [chr(i) for i in range(ord("a"), ord("z") + 1)]
 
 for b in range(0,1):
     for a in range(0,4):
         for i in range(0,6):
             if rd.randint(1,3) != 1:
-                if rd.randint(1,2) != 1:print(str(rd.choice(listAlphabet())), end="")
-                else:print(str(rd.choice(listAlphabet()).upper()), end="")
-            else:print(str(rd.randint(0,9)), end="")
-        print(" ", end="")    
-    print(" ")
+                if rd.randint(1,2) != 1:
+                    print(str(rd.choice(listAlphabet)), end="") #kleiner Alphabetischercharakter
+                else:
+                    print(str(rd.choice(listAlphabet).upper()), end="") #Großer Alphabetischercharakter
+            else:
+                print(str(rd.randint(0,9)), end="") #macht eine Zahl
+        print(" ", end="") #Leerzeichen
+    print(" ")  # Neue Zeile am Ende
+
+time.sleep(10)
